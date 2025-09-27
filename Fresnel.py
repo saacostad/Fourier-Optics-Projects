bfe4cc73bd13fd1863a49249c149014ae3e0d441
Santiago A <saacostad@unal.edu.co>
2025-09-27 18:56:49 -0500

    Added temporary mask support

--- Fresnel.py
+++ Fresnel.py
@@ -1,5 +1,6 @@
 import numpy as np 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+from numpy.matrixlib import matrix 
 
 
 
@@ -8,8 +9,10 @@ width = 481
 center = (width // 2) + 1
 lamb = 633e-6                   # Wavelenght He-Ne given in [mm]
 
-mask = np.zeros((width, width), dtype = bool)
 
+
+mask = np.zeros((width, width), dtype = bool)
+tempMask = np.zeros((width, width), dtype = int)
 
 
 size = 10
@@ -27,25 +30,41 @@ args1 = 10
 args2 = 20
 
 
-def modifyMask(matrix, shape, size = 10, position = (0, 0), kargs = None):
+def modifyMask(matrix, shape, size = 10, position = (0, 0), kargs = None, mode = 1):
     """ Taking an already existing mask, it modifies it by creating a hole according to the given paramenters """    
     match shape:
         case "square":
 
-           matrix[(center-position[0]) - size:(center-position[0]) + size, (center-position[1]) - size:(center-position[1]) + size] = 1
+           matrix[(center-position[0]) - size:(center-position[0]) + size, (center-position[1]) - size:(center-position[1]) + size] = mode
 
         case "circle":
 
             Y, X = np.ogrid[:width, :width]
             prov = (X - (center + position[0]))**2 + (Y - (center + position[1]))**2 <= size**2
 
-            matrix[::, ::] = np.logical_or(matrix, prov)
+            matrix[::, ::] = np.logical_or(matrix, prov) * mode 
 
         case "rectangle":
 
            sizex, sizey = kargs[0], kargs[1]
-           matrix[(center-position[0]) - sizex:(center-position[0]) + sizex, (center-position[1]) - sizey:(center-position[1]) + sizey] = 1
+           matrix[(center-position[0]) - sizex:(center-position[0]) + sizex, (center-position[1]) - sizey:(center-position[1]) + sizey] = mode 
 
+
+
+def modifyTempMask(tempMask, shape, size, position = (0, 0), kargs = None):
+    modifyMask(tempMask, shape, size, position, kargs, mode = 1)
+
+
+
+def createGrid(matrix, shape, size, reps, separation, arg1 = None, args2 = None):
+    """ Creates a grid of a given shape """
+    return None 
+
+
+    
+    
+
+    
 
 
 
